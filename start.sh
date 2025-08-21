72d0dc21c8ef4d11f56c5a55ac9911c0e47c5824
Emanuele Panizio <panc86@gmail.com>
2025-08-21 11:49:58 +0400

    start development environment from mounted workspace dir

--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-PROJ_PATH="${1:?ARG 1 (PROJ_PATH) is required}"
-PROJ_NAME="${PROJ_PATH##*/}"
-
-echo "Starting $PROJ_NAME in $PROJ_PATH..."
-mkdir -p $PROJ_PATH
-
+WSDIR="${HOME}/ws"
+mkdir -p ${WSDIR}
+IMAGE="denvpy:${PYTHON_VERSION:-3.12-slim}"
+echo "$IMAGE -> starting development environment"
 docker run --rm -it \
-    --name $PROJ_NAME \
+    --name denvpy-ws \
     --network host \
-    -v ${PROJ_PATH}:/data/${PROJ_NAME} \
-    denv:v1
+    -v ${WSDIR}:/app/ws \
+    $IMAGE \
+    $@
